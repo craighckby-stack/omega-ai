59ff5a9126f82fe11bf0f19fb8885e0a16d56456
CRAIG HUCKERBY <craighckby@gmail.com>
2026-01-08 00:26:24 +1000

    DARLIK_KHAN: EVOLVE

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,50 +2,40 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: dirname(fileURLToPath(import.meta.url)),
 });
 
-const eslintConfig = [
+const OFF = "off";
+
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      // TypeScript 相关规则
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/ban-ts-comment": "off",
-      "@typescript-eslint/prefer-as-const": "off",
-      
-      // React 相关规则
-      "react-hooks/exhaustive-deps": "off",
-      "react/no-unescaped-entities": "off",
-      "react/display-name": "off",
-      "react/prop-types": "off",
-      
-      // Next.js 相关规则
-      "@next/next/no-img-element": "off",
-      "@next/next/no-html-link-for-pages": "off",
-      
-      // 一般JavaScript规则
-      "prefer-const": "off",  // 关闭prefer-const规则
-      "no-unused-vars": "off",
-      "no-console": "off",
-      "no-debugger": "off",
-      "no-empty": "off",
-      "no-irregular-whitespace": "off",
-      "no-case-declarations": "off",
-      "no-fallthrough": "off",
-      "no-mixed-spaces-and-tabs": "off",
-      "no-redeclare": "off",
-      "no-undef": "off",
-      "no-unreachable": "off",
-      "no-useless-escape": "off",
+      "@typescript-eslint/no-explicit-any": OFF,
+      "@typescript-eslint/no-unused-vars": OFF,
+      "@typescript-eslint/no-non-null-assertion": OFF,
+      "@typescript-eslint/ban-ts-comment": OFF,
+      "@typescript-eslint/prefer-as-const": OFF,
+      "react-hooks/exhaustive-deps": OFF,
+      "react/no-unescaped-entities": OFF,
+      "react/display-name": OFF,
+      "react/prop-types": OFF,
+      "@next/next/no-img-element": OFF,
+      "@next/next/no-html-link-for-pages": OFF,
+      "prefer-const": OFF,
+      "no-unused-vars": OFF,
+      "no-console": OFF,
+      "no-debugger": OFF,
+      "no-empty": OFF,
+      "no-irregular-whitespace": OFF,
+      "no-case-declarations": OFF,
+      "no-fallthrough": OFF,
+      "no-mixed-spaces-and-tabs": OFF,
+      "no-redeclare": OFF,
+      "no-undef": OFF,
+      "no-unreachable": OFF,
+      "no-useless-escape": OFF,
     },
   },
 ];
-
-export default eslintConfig;
